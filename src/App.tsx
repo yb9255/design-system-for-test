9879661a05297ecabff17dc850654941ff17a903
yb9255 <yoobin9255@gmail.com>
2022-10-21 18:22:36 +0900

    made simple form for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Input from "./components/Input";
+import Form from "./components/Form";
+import Button from "./components/Button";
 
 export type TestType = {
   test1: string;
@@ -10,15 +12,21 @@ function App() {
   const [value, setValue] = useState("");
 
   return (
-    <>
+    <Form
+      onSubmit={(event) => {
+        event.preventDefault();
+        alert(value);
+      }}
+    >
       <Input
         value={value}
         onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
           setValue(event.target.value)
         }
       />
+      <Button name="Submit" />
       <h1>{value}</h1>
-    </>
+    </Form>
   );
 }
 
