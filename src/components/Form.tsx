dffe552549ad4531c2545c8547cc31dfc3b2c4ad
yoobin9255 <yoobin9255@cclss.net>
2022-10-21 18:15:06 +0900

    test like common production

---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components/macro";
+
+interface FormType {
+  onSubmit(event: React.FormEvent<HTMLFormElement>): void;
+  children: React.ReactNode;
+}
+
+function Form({ onSubmit, children }: FormType) {
+  return <S_Form onSubmit={onSubmit}>{children}</S_Form>;
+}
+
+const S_Form = styled.form``;
